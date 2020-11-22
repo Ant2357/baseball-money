5ca9475ade8e3955f18c0daad8b7bf08ce400fb5
Ant <ryu4979@gmail.com>
2020-11-22 22:08:13 +0900

    feat: 投手の年俸計算処理とdefPlayerを追加

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ import Lib
 
 main :: IO ()
 main = do
-  let player = Player {
+  let batterPlayer = defPlayer {
     name         = "坂口 智隆",
     age          = 36,
     g            = 114,
@@ -15,5 +15,21 @@ main = do
     isFirstPlace = False
   }
 
-  let annualSalary = baseballMoney "batter" player
-  putStrLn $ show (10000 * annualSalary) ++ "JPY" 
+  let pitcherPlayer = defPlayer {
+    name         = "石山 泰稚",
+    age          = 32,
+    ip           = 44.2,
+    era          = 2.01,
+    sv           = 20,
+    isForeigner  = False,
+    isFirstPlace = False
+  }
+
+
+  -- 打者年俸
+  let batterSalary = baseballMoney "batter" batterPlayer
+  putStrLn $ show (10000 * batterSalary) ++ "JPY"
+
+  -- 投手年俸
+  let pitcherSalary = baseballMoney "pitcher" pitcherPlayer
+  putStrLn $ show (10000 * pitcherSalary) ++ "JPY"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( Player (..)
+    , defPlayer
     , baseballMoney
     ) where
 
@@ -10,9 +13,25 @@ data Player = Player {
   age          :: Int,
   g            :: Int,
   obp          :: Float,
+  ip           :: Float,
+  era          :: Float,
+  sv           :: Int,
   isForeigner  :: Bool,
   isFirstPlace :: Bool
 } deriving (Show)
+
+defPlayer :: Player
+defPlayer = Player {
+  name         = "",
+  age          = 20,
+  g            = 0,
+  obp          = 0.0,
+  ip           = 0.0,
+  era          = 0.0,
+  sv           = 0,
+  isForeigner  = False,
+  isFirstPlace = False
+}
 
 baseballMoney :: String -> Player -> Int
 baseballMoney baseballType player = 
@@ -23,4 +42,11 @@ baseballMoney baseballType player =
       + (if isForeigner player then 1 else 0) * 9753
       + (if isFirstPlace player then 1 else 0) * 1685
       - 13322
+    "pitcher" -> age player * 858
+      + round (ip player * 66)
+      + round (era player * (-134))
+      + (if isForeigner player then 1 else 0) * 6438
+      + (if isFirstPlace player then 1 else 0) * 4007
+      + sv player * 265
+      - 21880
     _ -> 0
