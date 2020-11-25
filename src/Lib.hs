57fa468cd6c5fcf6dde3afdc60d1a3e54be78633
Ant <ryu4979@gmail.com>
2020-11-25 17:10:24 +0900

    fix: Lib.hsをﾁｮｯﾄﾘﾌｧｸﾀﾘﾝｸﾞ

--- src/Lib.hs
+++ src/Lib.hs
@@ -34,19 +34,19 @@ defPlayer = Player {
 }
 
 baseballMoney :: String -> Player -> Int
-baseballMoney baseballType player = 
-  case baseballType of
-    "batter" -> age player * 415
-      + g player * 85
-      + round (obp player * 5218.0)
-      + (if isForeigner player then 1 else 0) * 9753
-      + (if isFirstPlace player then 1 else 0) * 1685
-      - 13322
-    "pitcher" -> age player * 858
-      + round (ip player * 66)
-      + round (era player * (-134))
-      + (if isForeigner player then 1 else 0) * 6438
-      + (if isFirstPlace player then 1 else 0) * 4007
-      + sv player * 265
-      - 21880
-    _ -> 0
+baseballMoney "batter" player =
+  age player * 415
+  + g player * 85
+  + round (obp player * 5218.0)
+  + (if isForeigner player then 1 else 0) * 9753
+  + (if isFirstPlace player then 1 else 0) * 1685
+  - 13322
+baseballMoney "pitcher" player =
+  age player * 858
+  + round (ip player * 66)
+  + round (era player * (-134))
+  + (if isForeigner player then 1 else 0) * 6438
+  + (if isFirstPlace player then 1 else 0) * 4007
+  + sv player * 265
+  - 21880
+baseballMoney _ player = 0
